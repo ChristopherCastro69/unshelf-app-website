a17380649610e63f89fbc39146cd09c7701f08da
Topitops69 <topitops123@gmail.com>
2024-06-08 12:00:39 +0800

    update

--- src/components/Unshelf/Hero.jsx
+++ src/components/Unshelf/Hero.jsx
@@ -8,7 +8,10 @@ const Hero = () => {
         <h1 className="h1 hero-title">
           The Ultimate Food Saver for a Sustainable Future
         </h1>
-        <a href="">
+        <a
+          href="https://github.com/ChristopherCastro69/unshelf-app-website/blob/unshelf/src/assets/apk/UNSHELF.zip"
+          download="UNSHELF.zip"
+        >
           <button className="btn-primary">Download now</button>
         </a>
       </div>
